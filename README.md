09aa46f3fd3761662d2a5fa5a7fd5bb15afc9981
thoughtp0lice <dengchigui@gmail.com>
2024-08-06 15:31:50 -0500

    llm test and doc

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 ## Setup
 
+### Conda environment
+
 Setup environment with `conda env create -f environment.yml`
+
+### Ollama server
+
+LLM is set up with ollama which is a local web server built on top of llama cpp. It automatically takes care of templates, model download, image loading and etc. 
+
+Mac download with home brew is available. Can also download from https://ollama.com/library. 
+
+Documents for ollama is [here](https://github.com/ollama/ollama/tree/main/docs). We are using [ollama python api](https://github.com/ollama/ollama-python), with usage examples [here](https://github.com/ollama/ollama-python/tree/main/examples)
--- llm/ollama_engine.py
+++ llm/ollama_engine.py
@@ -1,9 +1,9 @@
 from typing import Optional, Dict
 import ollama
-from llm_utils import llm_typing
+from llm_utils import llm_typing, path
 
 
-class Ollama:
+class OllamaEngine:
 
     def __init__(self, model: str, generation_options: Dict[str, str]):
         self._model = model
@@ -15,7 +15,20 @@ class Ollama:
     ):
         response = ollama.chat(
             model=self._model,
-            messages=message,  # type: ignore
+            messages=messages,  # type: ignore
             options=self._options  # type: ignore
         )  # type: ignore
-        return response[message]
+        return response["message"]
+
+
+if __name__ == "__main__":
+    engine = OllamaEngine("moondream", {})
+    res = engine.chat_completion([{
+        "role":
+        "user",
+        "content":
+        "what is in this image?",
+        "images":
+        [path.working_dir_path("llm/prompts/img/bobs_burgers_promo.png")]
+    }])
+    print(res)
